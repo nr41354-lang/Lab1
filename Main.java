78e32a6abcd54999221a16d5f5fce739a7c4ca56
nr41354-lang <nr41354@gmail.com>
2025-10-30 02:20:25 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,66 +1,32 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-import java.util.Scanner;
+mport java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
+        System.out.print("Введите грузоподъёмность X и веса A, B, C: ");
+        int X = in.nextInt(), A = in.nextInt(), B = in.nextInt(), C = in.nextInt();
 
-        System.out.print("Введите грузоподъёмность лифта X: ");
-        int X = in.nextInt();
-
-        System.out.print("Введите вес груза A: ");
-        int A = in.nextInt();
-
-        System.out.print("Введите вес груза B: ");
-        int B = in.nextInt();
-
-        System.out.print("Введите вес груза C: ");
-        int C = in.nextInt();
-
-        int maxCount = 0;
         String comb = "";
+        int max = 0;
 
-        // Проверка грузов по отдельности
-        if (A <= X && 1 > maxCount) {
-            maxCount = 1;
-            comb = "A";
+        if (A + B + C <= X) { comb = "A B C"; max = 3; }
+        else if ((A + B <= X) || (A+C<=X)||(B + C <= X)) {
+            max = 2;
+            if (A + B <= X) comb = "A B";
+            else if (A + C <= X) comb = "A C";
+            else comb = "B C";
         }
-        if (B <= X && 1 > maxCount) {
-            maxCount = 1;
-            comb = "B";
-        }
-        if (C <= X && 1 > maxCount) {
-            maxCount = 1;
-            comb = "C";
-        }
-
-        // Проверка комбинаций из 2 грузов
-        if (A + B <= X && 2 > maxCount) {
-            maxCount = 2;
-            comb = "A B";
-        }
-        if (A + C <= X && 2 > maxCount) {
-            maxCount = 2;
-            comb = "A C";
-        }
-        if (B + C <= X && 2 > maxCount) {
-            maxCount = 2;
-            comb = "B C";
+        else if ((A <= X)||(B<=X)||(C <= X)) {
+            max = 1;
+            if (A <= X) comb = "A";
+            else if (B <= X) comb = "B";
+            else comb = "C";
         }
 
-        //  Проверка омбинации из трёх грузов
-        if (A + B + C <= X && 3 > maxCount) {
-            maxCount = 3;
-            comb = "A B C";
-        }
-
-        // Вывод результата
-        if (maxCount == 0) {
-            System.out.println("В лифт нельзя загрузить ни один груз без превышения массы.");
-        } else {
-            System.out.println("Можно загрузить груз(ы): " + comb);
-            System.out.println("Количество грузов, которые можно поместить: " + maxCount);
-        }
+        System.out.println(max == 0
+                ? "В лифт нельзя загрузить ни один груз без превышения массы."
+                : "Можно загрузить: " + comb + " (" + max + ")");
     }
 }
